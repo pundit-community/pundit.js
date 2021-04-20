4216f337628091ddcce383ef6e2c45838d30dd7e
John Otander <johnotander@gmail.com>
2021-04-20 14:25:09 -0600

    Spike out react bindings

--- example/index.tsx
+++ example/index.tsx
@@ -1,13 +1,22 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { PunditProvider, When } from '../react';
+import { Policy } from '../.';
+
+const user = {};
+const record = {};
+const policy = new Policy(user, record);
+policy.add('view', () => true);
+policy.add('edit', () => false);
 
 const App = () => {
   return (
-    <div>
-      <Thing />
-    </div>
+    <PunditProvider policy={policy}>
+      Abilities:
+      <When can="view">view</When>
+      <When can="edit">edit</When>
+    </PunditProvider>
   );
 };
 
--- src/react.tsx
+++ src/react.tsx
@@ -1,6 +1,21 @@
 import * as React from 'react';
 
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
+const PunditContext = React.createContext();
+
+export const usePundit = () => {
+  const value = React.useContext(PunditContext);
+  return value;
+};
+
+export const PunditProvider = ({ user, record, policy, children }) => {
+  return (
+    <PunditContext.Provider value={{ user, record, policy }}>
+      {children}
+    </PunditContext.Provider>
+  );
+};
+
+export const When = ({ can, children }) => {
+  const { policy } = usePundit();
+  const canPerformAction = policy.can(can);
 };
