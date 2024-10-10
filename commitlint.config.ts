151ac79948e16823687802e4c8c9a5b26894c077
Chris Alley <chris@chrisalley.info>
2024-10-10 19:33:06 +1300

    ci(commitlint): support longer commit messages from Dependabot

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,7 +1,10 @@
 import type { UserConfig } from '@commitlint/types'
 
 const Configuration: UserConfig = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [2, 'always', 1000] // Support longer commit messages from Dependabot
+  }
 }
 
 export default Configuration
